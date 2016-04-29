7314fb4bcdee2ec6d9f8bf91a222c5cd8314405b
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-28 20:09:02 -0700

    Added calculations on form submit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 angular.module('myApp', ['ngMessages'])
   .controller('myCtrl', function($scope){
-    $scope.subTotal = '0.00';
-    $scope.tip = '0.00';
-    $scope.total = '0.00';
-    $scope.tipTotal = '0.00';
+    $scope.subTotal = 0.00;
+    $scope.tip = 0.00;
+    $scope.total = 0.00;
+    $scope.tipTotal = 0.00;
     $scope.mealCount = 0;
-    $scope.avgTip = '0.00';
+    $scope.avgTip = 0.00;
+    $scope.allMeals = [];
 
+    //Resets input values, used when site first loads and 
+    //after each successful form submission
     var setValues = function(){
       $scope.price = '';
       $scope.taxRate = '';
@@ -16,13 +19,22 @@ angular.module('myApp', ['ngMessages'])
 
     setValues();
 
+    //Form submission logic
     $scope.submit = function(){
-      if(!$scope.userForm.$error.required){
+      //Checks to see if form is valid
+      if(!$scope.mealForm.$error.required){
         $scope.formValid = true;
       }
+      //If form is valid calculate charges
       if($scope.formValid){
-
+        $scope.subTotal = $scope.price + (($scope.price * $scope.taxRate) / 100);
+        $scope.tip = ($scope.price * $scope.tips) / 100;
+        $scope.total = $scope.subTotal + $scope.tip;
+        $scope.tipTotal = $scope.tipTotal + $scope.tip;
+        $scope.mealCount++;
+        $scope.avgTip = $scope.tipTotal / $scope.mealCount;
+        $scope.mealForm.$setPristine();
         setValues();
       }
-    }
+    };
   });
